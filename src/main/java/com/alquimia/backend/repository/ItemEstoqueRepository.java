4c3b0b09bd50a809071202795a6bf85b294b01ed
JUDE VIEIRA DE LIMA <jude_lima@estudante.sesisenai.org.br>
2025-10-09 14:36:33 -0300

    adicionando model e repository

---
+++ src/main/java/com/alquimia/backend/repository/ItemEstoqueRepository.java
@@ -0,0 +1,6 @@
+package com.alquimia.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemEstoque extends JpaRepository<ItemEstoque, Integer> {
+}
