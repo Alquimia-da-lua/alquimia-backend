f6022cffa78fcda22d8b8c1ec632514c6a370e33
judevieira <judevieiradlima@gmail.com>
2025-10-13 19:47:18 -0300

    alteração dos models estoque e itemEstoque

--- src/main/java/com/alquimia/backend/model/Estoque.java
+++ src/main/java/com/alquimia/backend/model/Estoque.java
@@ -1,25 +1,47 @@
 package com.alquimia.backend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "estoque")
-@Data
+@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Estoque {
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer cdEstoque;
 
-    @Column(nullable = false, columnDefinition = "boolean default true")
-    private Boolean isAtivo;
+    @Column(nullable = false)
+    private Boolean isAtivo = true;
 
-    private List<ItemEstoque> itens;
+    @OneToMany(
+            mappedBy = "cdEstoque", //cdEstoque do itemEstoque
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<ItemEstoque> itens = new ArrayList<>();
+
+    @PrePersist
+    public void prePersist(){
+        if (isAtivo==null) isAtivo = true;
+    }
+
+    //mantendo lados sincronizados
+    public void adicionarItem (ItemEstoque item){
+        itens.add(item);
+        item.setCdEstoque(this); //referencia o estoque que o item vai pertencer
+
+    }
+
+    public void removerItem (ItemEstoque item){
+        itens.remove(item);
+        item.setCdEstoque(null); //item passa a nao possuir estoque
+    }
+
 
 }
--- src/main/java/com/alquimia/backend/model/ItemEstoque.java
+++ src/main/java/com/alquimia/backend/model/ItemEstoque.java
@@ -1,29 +1,44 @@
 package com.alquimia.backend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Min;
+import lombok.*;
 
+@Entity
 @Table(name = "item_estoque")
-@Data
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemEstoque {
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer cdItemEstoque;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) //fetch so busca do banco quando acessar o relacionamento
     @JoinColumn(name = "cd_produto", nullable = false)
-    @Column(nullable = false)
     private Produto cdProduto;
 
-    @ManyToOne
-    @JoinColumn(name = "cd_estoque")
-    @Column(nullable = false)
-    private Estoque cdEstoque;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "cd_estoque", nullable = false)
+    private Estoque cdEstoque; //mapped by do estoque
 
+    @Min(0)
     @Column(nullable = false)
-    private Integer qtItemEstoque;
+    private Integer qtItemEstoque = 0;
+
+    public void aumentarQtdeItemEstoque(int qtde){
+        if (qtde < 0) throw new IllegalArgumentException("Quantidade negativa!");
+
+        this.qtItemEstoque += qtde;
+    }
+
+    public void reduzirQtdeItemEstoque(int qtde){
+        if (qtde < 0) throw new IllegalArgumentException("Quantidade negativa.");
+
+        int restante = this.qtItemEstoque - qtde;
+        if (restante < 0) throw new IllegalArgumentException("estoque insuficiente.");
+
+        this.qtItemEstoque = restante;
+    }
+
 }
