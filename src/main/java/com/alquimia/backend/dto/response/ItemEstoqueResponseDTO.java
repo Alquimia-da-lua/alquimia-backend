57a83271e2488e8850f1581aa590e40f2366286b
judevieira <judevieiradlima@gmail.com>
2025-10-14 13:08:53 -0300

    finalizando estoqueService e iniciando ItemEstoqueService

--- src/main/java/com/alquimia/backend/dto/response/ItemEstoqueResponseDTO.java
+++ src/main/java/com/alquimia/backend/dto/response/ItemEstoqueResponseDTO.java
@@ -1,10 +1,5 @@
 package com.alquimia.backend.dto.response;
 
-import com.alquimia.backend.model.Estoque;
-import com.alquimia.backend.model.Produto;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 
 public record ItemEstoqueResponseDTO(
         Integer cdItemEstoque,
