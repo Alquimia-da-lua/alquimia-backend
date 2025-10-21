06c7c93fc9b56957e29af26b598a31abe7dd61e0
samara_bento <samaradesouzabento@gmail.com>
2025-10-21 14:19:06 -0300

    feat/ Ajuste na ItemPedidoRepository

--- src/main/java/com/alquimia/backend/repository/ItemPedidoRepository.java
+++ src/main/java/com/alquimia/backend/repository/ItemPedidoRepository.java
@@ -10,11 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface ItemPedidoRepository  extends JpaRepository<ItemPedido, Integer> {
-    Optional<ItemPedido>cadastrarItemPedido(Integer cdItemPedido, Integer qtItemPedido);
-    Optional<ItemPedido>alterarItemPedido(Integer cdItemPedido);
-    Optional<ItemPedido>deletarItemPedido(Integer cdItemPedido);
-    Optional<ItemPedido>listarItemPedido(Integer cdItemPedido);
     Optional<ItemPedido> findAllByCdItemPedido(Integer cdItemPedido);
-    Optional<ItemPedido> findAllByCdPedido(Integer cdPedido);
     List<ItemPedido> findAllByCdPedido(Pedido cdPedido);
 }
